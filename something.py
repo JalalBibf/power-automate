31f95004ee5522b9789debc01d9e37d815ca45d0
JalalBibf <122723104+JalalBibf@users.noreply.github.com>
2023-01-31 19:21:52 +0300

    New features Added 2! 🐱‍🏍💥🚀🎊🧨👌

--- something.py
+++ something.py
@@ -4,11 +4,9 @@ for i in range(1000, 700000):
         num = list(map(int, str(i)))
         if len(num) == 4:
             if (num[0] + num [3]) == (num[1] + num[2]):
-                # print(num)
                 completerNums.append(i)
         elif len(num) == 6:
             if (num[0] + num [5]) == (num[1] + num[4]) == (num[2] + num[3]):
-                # print(num)
                 completerNums.append(i)
 print(completerNums)
 print(len(completerNums))
