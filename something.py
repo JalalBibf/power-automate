00ab141a7d190fc68448c322d391113d53b2771f
JalalBibf <122723104+JalalBibf@users.noreply.github.com>
2023-01-31 17:08:21 +0300

    Add files via upload

---
+++ something.py
@@ -0,0 +1,16 @@
+completerNums = []
+for i in range(1000, 700000):
+    if(len(str(i))%2)==0:
+        num = list(map(int, str(i)))
+        if len(num) == 4:
+            if (num[0] + num [3]) == (num[1] + num[2]):
+                # print(num)
+                completerNums.append(i)
+        elif len(num) == 6:
+            if (num[0] + num [5]) == (num[1] + num[4]) == (num[2] + num[3]):
+                # print(num)
+                completerNums.append(i)
+print(completerNums)
+print(len(completerNums))
+# print(completerNums.index(123321))
+    
